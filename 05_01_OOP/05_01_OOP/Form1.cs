6482b1bd8afaa86f83e5fd23572e07798fc290cd
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2020-10-21 10:52:12 +0200

    Argomento nuovo es 1

--- 05_01_OOP/05_01_OOP/Form1.cs
+++ 05_01_OOP/05_01_OOP/Form1.cs
@@ -12,25 +12,26 @@ namespace _05_01_OOP
 {
     public partial class Form1 : Form
     {
-        Rectangle r = new Rectangle();
+        
 
         public Form1()
         {
             InitializeComponent();
-
-
         }
         private void btncrea_Click(object sender, EventArgs e)
         {
-            r.b = Convert.ToInt32(textBox1.Text);
+            /*r.b = Convert.ToInt32(textBox1.Text);
             r.h = Convert.ToInt32(textBox2.Text);
             Rectangle t;
             t = r;
             MessageBox.Show("base:" + r.b.ToString() + "\naltezza:" + r.h.ToString());
             MessageBox.Show("base:" + t.b.ToString() + "\naltezza:" + t.h.ToString());
-
-
-
+            */
+            Rectangle r = new Rectangle(Convert.ToInt32(textBox1.Text),Convert.ToInt32(textBox2.Text));
+            r.Colore = Color.Orange;
+            MessageBox.Show(r.getsides());
         }
+
+
     }
 }
--- 05_01_OOP/05_01_OOP/Rectangle.cs
+++ 05_01_OOP/05_01_OOP/Rectangle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,22 @@ namespace _05_01_OOP
 {
     class Rectangle
     {
-        public int b, h;
+        private int b, h;
+        public Color Colore;
+        public Rectangle()
+        {
+            b = 1;
+            h = 2;
+        }
+        public Rectangle(int x, int j)
+        {
+            b = x;
+            h = j;
+        }
+        public string getsides()
+        {
+            return b.ToString() + " " + h.ToString();
+        }
     }
 }
+
