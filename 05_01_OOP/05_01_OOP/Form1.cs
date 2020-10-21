b9eb558f0b7e6c44b6ffe7957ec25ee514a4c135
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2020-10-21 10:26:22 +0200

    Argomento nuovo es 1

---
+++ 05_01_OOP/05_01_OOP/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _05_01_OOP
+{
+    public partial class Form1 : Form
+    {
+        Rectangle r = new Rectangle();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+        private void btncrea_Click(object sender, EventArgs e)
+        {
+            r.b = Convert.ToInt32(textBox1.Text);
+            r.h = Convert.ToInt32(textBox2.Text);
+            Rectangle t;
+            t = r;
+            MessageBox.Show("base:" + r.b.ToString() + "\naltezza:" + r.h.ToString());
+            MessageBox.Show("base:" + t.b.ToString() + "\naltezza:" + t.h.ToString());
+
+
+
+        }
+    }
+}
---
+++ 05_01_OOP/05_01_OOP/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_01_OOP
+{
+    class Rectangle
+    {
+        public int b, h;
+    }
+}
