0df727f5e4aed51b74f7510e752dbf72b2749444
giordanopaolo <62544238+giordanopaolo@users.noreply.github.com>
2020-11-19 20:26:35 +0100

    Compito pila per il 20/11/2020

---
+++ 16_EsercizioPila/16_EsercizioPila/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace _16_EsercizioPila
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.peso = new System.Windows.Forms.TextBox();
+            this.tara = new System.Windows.Forms.TextBox();
+            this.cod = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Carica";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 41);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Scarica";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // peso
+            // 
+            this.peso.Location = new System.Drawing.Point(206, 12);
+            this.peso.Name = "peso";
+            this.peso.Size = new System.Drawing.Size(100, 23);
+            this.peso.TabIndex = 1;
+            // 
+            // tara
+            // 
+            this.tara.Location = new System.Drawing.Point(206, 42);
+            this.tara.Name = "tara";
+            this.tara.Size = new System.Drawing.Size(100, 23);
+            this.tara.TabIndex = 2;
+            // 
+            // cod
+            // 
+            this.cod.Location = new System.Drawing.Point(206, 72);
+            this.cod.Name = "cod";
+            this.cod.Size = new System.Drawing.Size(100, 23);
+            this.cod.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Peso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tara";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(168, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Cod";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 113);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cod);
+            this.Controls.Add(this.tara);
+            this.Controls.Add(this.peso);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox peso;
+        private System.Windows.Forms.TextBox tara;
+        private System.Windows.Forms.TextBox cod;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ 16_EsercizioPila/16_EsercizioPila/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _16_EsercizioPila
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public struct Containe
+        {
+            public string Cod;
+            public string peso;
+            public string tara;
+        }
+        Stack<Containe> container = new Stack<Containe>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Containe c;
+            c.Cod = cod.Text;
+            c.peso = peso.Text;
+            c.tara = tara.Text;
+            container.Push(c);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(container.Count != 0)
+            {
+                Containe c = container.Peek();
+                MessageBox.Show("Cod" + c.Cod + "\nPeso:" + c.peso + "\nTara:" + c.tara);
+                container.Pop();
+            }
+            else MessageBox.Show("non ci sono più container");
+
+
+        }
+    }
+}
