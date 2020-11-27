d9ce772059fa39a84f0f45198859893775f20a18
giordanopaolo <62544238+giordanopaolo@users.noreply.github.com>
2020-11-27 09:16:11 +0100

    ereditarità

---
+++ 17_Ereditarietà/17_Ereditarietà/Mannaro.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17_Ereditarietà
+{
+    class Mannaro : Mostro
+    {
+        protected bool mutato;
+        public Mannaro()
+        {
+            punti = 33;
+            verso = "ululato";
+            mutato = true;
+        }
+        public double Valore()
+        {
+            return (mutato)? 2 * base.Valore():base.Valore();
+        }
+        public string Verso()
+        {
+            return (mutato) ? verso : base.verso;
+        }
+        public void Muta()
+        {
+            mutato = !mutato;
+        }
+    }
+}
---
+++ 17_Ereditarietà/17_Ereditarietà/Mostro.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _17_Ereditarietà
+{
+    class Mostro
+    {
+        protected double punti;
+        protected string verso;
+        public Mostro()
+        {
+            punti = 25;
+            verso = "grugnito";
+        }
+        public double Valore()
+        {
+            return punti;
+        }
+        public string Verso()
+        {
+            return verso;
+        }
+    }
+}
