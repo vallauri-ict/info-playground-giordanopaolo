5093cb05c079b39b2e0c9def7395a8e0a8dcaed4
giordanopaolo <62544238+giordanopaolo@users.noreply.github.com>
2020-12-02 10:54:22 +0100

    esercizio dato il 02/12/2020

---
+++ 19_ereditarietà_esercizio_guidato_finale/19_ereditarietà_esercizio_guidato_finale/Mannaro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _19_ereditarietà_esercizio_guidato_finale
+{
+    public class Mannaro : Mostro
+    {
+        new public string nome;
+        public Mannaro(): base()
+        {
+            this.nome = "Mannaro";
+        }
+        public Mannaro(string nome): base(nome)
+        {
+            this.nome = "Mannaro" + nome;
+        }
+        new public void Rivela()
+        {
+            string s = nome + (char)(32);
+            System.Windows.Forms.MessageBox.Show(s + base.nome);
+        }
+    }
+}
---
+++ 19_ereditarietà_esercizio_guidato_finale/19_ereditarietà_esercizio_guidato_finale/Mostro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _19_ereditarietà_esercizio_guidato_finale
+{
+    public class Mostro
+    {
+        public string nome;
+        public Mostro(): this("Mostro")
+        {
+
+        }
+        public Mostro(string nome)
+        {
+            this.nome = nome;
+        }
+        public void Rivela()
+        {
+            System.Windows.Forms.MessageBox.Show(nome);
+        }
+    }
+}
