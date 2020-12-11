8ef27993b38420b805740cef174a48991c4a6802
giordanopaolo <62544238+giordanopaolo@users.noreply.github.com>
2020-12-11 09:56:49 +0100

    Esercizio sulle classi derivate del 11/12/2020

---
+++ 20_Classiderivate/20_Classiderivate/Persona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Classiderivate
+{
+    
+    class Persona
+    {
+        private string nome, cognome, sesso;
+        private int eta;
+        private void setAtt(string nome, string cognome, int eta, string sesso)
+        {
+            this.sesso = sesso;
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+        }
+        public string getnome()
+        {
+            return this.nome;
+        }
+        public string getcognome()
+        {
+            return this.cognome;
+        }
+        public string getsesso()
+        {
+            return this.sesso;
+        }
+        public int geteta()
+        {
+            return this.eta;
+        }
+    }
+}
---
+++ 20_Classiderivate/20_Classiderivate/Studente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_Classiderivate
+{
+    class Studente : Persona
+    {
+        List<int> voti = new List<int>();
+        public double Media()
+        {
+            int sommaVoti = 0;
+            foreach (var item in voti)
+            {
+                sommaVoti += item;
+            }
+            return (double)sommaVoti * voti.Count;
+        }
+    }
+}
