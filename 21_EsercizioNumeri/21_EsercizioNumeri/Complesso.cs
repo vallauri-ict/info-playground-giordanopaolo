65a171bcd5db818a7674a419876324633fa541f1
giordanopaolo <62544238+giordanopaolo@users.noreply.github.com>
2020-12-18 09:41:34 +0100

    Inizio esercizio sui quaternioni

---
+++ 21_EsercizioNumeri/21_EsercizioNumeri/Complesso.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_EsercizioNumeri
+{
+    class Complesso
+    {
+        private double immaginario;
+        private double reale;
+        public Complesso():this(1)
+        {
+
+        }
+        public Complesso(double immaginario):this(1,immaginario)
+        {
+
+        }
+        public Complesso(double reale,double immaginario)
+        {
+            Immaginario = immaginario;
+            Reale = reale;
+        }
+
+        
+        public double Immaginario { get => immaginario; set => immaginario = value; }
+        
+        public double Reale { get => reale; set => reale = value; }
+        public void Modulo()
+        {
+
+        }
+        public void Coniugato()
+        {
+
+        }
+    }
+}
---
+++ 21_EsercizioNumeri/21_EsercizioNumeri/Quaternione.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_EsercizioNumeri
+{
+    class Quaternione : Complesso
+    {
+        private double j, k;
+
+        public double J { get => j; set => j = value; }
+
+        public double K { get => k; set => k = value; }
+        public Quaternione():this(1)
+        {
+
+        }
+        public Quaternione(double immaginario) : this(1,1)
+        {
+
+        }
+        public Quaternione(double immaginario,double reale) : this(1,1,1)
+        {
+
+        }
+        public Quaternione(double immaginario, double reale,double j) : this(1, 1, 1, 1)
+        {
+
+        }
+        public Quaternione(double immaginario, double reale, double j,double k) 
+        {
+
+        }
+    }
+}
