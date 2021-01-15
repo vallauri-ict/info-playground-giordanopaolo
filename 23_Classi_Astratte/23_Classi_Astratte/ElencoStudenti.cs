85b9322d4f0558cabaad72053f769b21dae367a4
giordanopaolo <62544238+giordanopaolo@users.noreply.github.com>
2021-01-15 09:57:03 +0100

    implementazione es 23 classi astratte

---
+++ 23_Classi_Astratte/23_Classi_Astratte/ElencoStudenti.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Classi_Astratte
+{
+    class ElencoStudenti : Report
+    {
+        List<Studente> lststud = new List<Studente>();
+        public string stampa = "";
+        protected override void printBody()
+        {
+            foreach (var item in lststud)
+            {
+                stampa += item.Nomecompleto() + "\n";
+            }
+        }
+        protected override void printFooter()
+        {
+            stampa += "FOOTER STAMPA STUDENTI\n";
+        }
+        protected override void printHeader()
+        {
+            stampa += "INTESTAZIONE STAMPA STUDENTI\n";
+        }
+        public ElencoStudenti()
+        {
+            lststud.Add(new Studente("Andrea", "Giaccardi"));
+            lststud.Add(new Studente("Andrea", "Giaccardi"));
+        }
+    }
+}
---
+++ 23_Classi_Astratte/23_Classi_Astratte/Studente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_Classi_Astratte
+{
+    class Studente
+    {
+        
+        private string Nome { get; set; }
+
+        private string Cognome { get; set; }
+
+        private DateTime datanascita;
+        public Studente(string nome, string cognome)
+        {
+            Nome = nome;
+            Cognome = cognome;
+        }
+        public string Nomecompleto()
+        {
+            return Nome + " " + Cognome;
+        }
+    }
+}
