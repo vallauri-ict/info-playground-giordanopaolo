6fafceca419b3dfa469e7b84e458e0f459a0fce7
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2021-02-10 09:50:29 +0100

    iplementazione completata es 28 interfacce 2

---
+++ 28_Interfaccia_People/28_Interfaccia_People/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _28_Interfaccia_People
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        Person[] vect = new Person[] { new Person("Mario Rossi", "Cuneo"),
+                                       new Person("Enrico Alessandrini", "Fossano"),
+                                       new Person("Ettore Scemo", "SaviCity"),
+                                       new Person("Giordy Anddicapattto", "SaviNord")};
+        
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "";
+            foreach (var item in vect)
+            {
+                s += item.ToString() + "\n";
+            }
+            MessageBox.Show(s);
+            Array.Sort(vect);
+            s = "";
+            foreach (var item in vect)
+            {
+                s += item.ToString() + "\n";
+            }
+            MessageBox.Show(s);
+        }
+    }
+}
---
+++ 28_Interfaccia_People/28_Interfaccia_People/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28_Interfaccia_People
+{
+    class Person : IComparable
+    {
+        private string nome;
+        private string citta;
+        public Person(string nome, string citta)
+        {
+            this.nome = nome;
+            this.citta = citta;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            Person p = obj as Person;
+            return string.Compare(nome, p.nome, true);
+        }
+        public override string ToString()
+        {
+            return nome + " " + citta;
+        }
+
+    }
+}
