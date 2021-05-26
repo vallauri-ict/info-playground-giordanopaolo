711e6ff0e3cccca1f4dbc58bb6e93d76bf2734d2
Stud. Paolo Giordano <p.giordano.1320@vallauri.net>
2021-05-26 09:56:13 +0200

    Aggiunto esercizio 29 intelligenze artificiali

---
+++ 29_taxiCheFaPredizioni/29_taxiCheFaPredizioni/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _29_taxiCheFaPredizioni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
